c956fc6d5124647d657ac48c9c2a17e81a1a20c6
Ikechukwu Okalia <dev@ikechukwuokalia.com>
2023-08-21 02:21:12 +0100

    Update Location.php

--- src/Location.php
+++ src/Location.php
@@ -16,7 +16,7 @@ class Location{
   public $longitude;
   public $errors = [];
 
-  function __construct(string $ip=''){
+  function __construct(string|null $ip=''){
     $this->_checkSession();
     $this->_instantiate($ip);
   }
@@ -27,7 +27,7 @@ class Location{
 		}
     $this->user = $session->name;
   }
-  function _instantiate(string $ip=''){
+  function _instantiate(string|null $ip=''){
     $info = new \Victorybiz\GeoIPLocation\GeoIPLocation();
     if( !empty($ip) && \filter_var($ip,FILTER_VALIDATE_IP) ){
       $info->setIP($ip);
@@ -41,24 +41,24 @@ class Location{
     $this->currency_symbol = $info->getCurrencySymbol();
     $this->latitude = $info->getLatitude();
     $this->longitude = $info->getLongitude();
-    if( \defined('MYSQL_DATA_DB') ){
-      $db = MYSQL_DATA_DB;
+    global $database, $conn;
+    $conn = !empty($conn) && $conn instanceof MySQLDatabase ? $conn : (!empty($database) && $database instanceof MySQLDatabase ? $database :null);
+    $data_db = \defined("MYSQL_DATA_DB") ? MYSQL_DATA_DB : (\function_exists("\\get_database") ? \get_database("data") : null);
+    if ( $conn && $data_db ) {
       $country_name = empty($this->country) ? null : \strtolower($this->country);
       $country_code = empty($this->country_code) ? null : \strtoupper($this->country_code);
       $state_name = empty($this->state) ? null : \strtolower($this->state);
       $city_name = empty($this->city) ? null : \strtolower($this->city);
-      $sql = "SELECT
-                     (SELECT code FROM `{$db}`.`state` WHERE LOWER(name) = '{$state_name}' LIMIT 1) AS state_code,
-                     (SELECT code FROM `{$db}`.`city` WHERE LOWER(name) = '{$city_name}' LIMIT 1) AS city_code
-              FROM  `{$db}`.`country`
-              LIMIT 1";
-      $found = ( new MultiForm($db,'country') )->findBySql($sql);
+      $found = (!empty($state_name) && !empty($city_name)) ? ( new MultiForm($data_db, 'countries', "code", $conn) )
+        ->findBySql("SELECT
+          (SELECT `code` FROM :db:.`states` WHERE LOWER(name) = '{$conn->escapeValue($state_name)}' LIMIT 1) AS state_code,
+          (SELECT `code` FROM :db:.`cities` WHERE LOWER(name) = '{$conn->escapeValue($city_name)}' LIMIT 1) AS city_code
+        FROM  :db:.`countries`
+        LIMIT 1") : null;
       if( $found ){
         $this->state_code = $found[0]->state_code;
         $this->city_code = $found[0]->city_code;
       }
-    }else{
-      $this->errors['Location'][] = [3,256,'MYSQL_DATA_DB => Data storage database is not defined, to ensure better accuracy of location information, complete following steps: \r\n 1. Define constance \'MYSQL_DATA_DB\' storing database name. \r\n 2. Create database with defined name. \r\n 3. Create/Populate table: countries [fields]: id - int(11), iso2 - char(2) [ISO2 country code], name - varchar(150) country name. \r\n 4. Create/Populate table: states [fields]: id - int(11), name - char(30) state name, country_id - int(11). \r\n 5. Create/Populate table: cities [fields]: id - int(11), name - char(30) city name, state_id - int(11). ',__FILE__,__LINE__];
     }
   }
 
